eed6bf9d3a4160609253dc1a1342f6baffc4b71c
romanboyko88 <catsgonewild2@gmail.com>
2021-01-19 16:50:45 -0600

    note added

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,8 @@
 public class Login {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        //new line
     }
 }
+// git add . ---. adding to staging area, if no message everything is good
+
