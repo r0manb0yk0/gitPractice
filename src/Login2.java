fa755039218ee055de8c00945a20785b60b0d117
romanboyko88 <catsgonewild2@gmail.com>
2021-01-19 18:15:31 -0600

    new line of code

--- src/Login2.java
+++ src/Login2.java
@@ -1,2 +1,10 @@
 public class Login2 {
+    // create class --> add
+    // commit
+    // push
+
+    public static void main(String[] args) {
+
+        System.out.println("new code------>");
+    }
 }
